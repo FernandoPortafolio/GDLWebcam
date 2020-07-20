cc68df4e854f8508f6ab5412eec0b414b48f69e1
Fernando Acosta <Fernando Acosta>
2020-07-20 14:21:39 -0500

    barra fija

--- js/pages/header.js
+++ js/pages/header.js
@@ -2,4 +2,24 @@ $(function () {
   'use strict';
   //lettering para el nombre del sitio
   $('.nombre-sitio h1').lettering();
+
+  //menu fijo
+  var windowHeight = $(window).height();
+  var barraAltura = $('.barra').height();
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > windowHeight) {
+      $('.barra').addClass('fixed');
+      $('body').css({ 'margin-top': barraAltura + 'px' });
+    } else {
+      $('.barra').removeClass('fixed');
+      $('body').css({ 'margin-top': '0' });
+    }
+  });
+
+  //menu responsive
+  $('.hamburger').click(function (e) {
+    e.preventDefault();
+    $('.navegacion').slideToggle();
+  });
 });
