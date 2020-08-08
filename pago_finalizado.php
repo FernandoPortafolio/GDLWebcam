26557cfbf3059922c0a9ac94cc59523709dc4a81
Fernando Acosta <Fernando Acosta>
2020-08-08 14:13:10 -0500

    redireccionado

--- pago_finalizado.php
+++ pago_finalizado.php
@@ -4,6 +4,30 @@
 <section class="seccion contenedor">
     <h2 class="separador justify-center">Pago Finalizado</h2>
 
+    <?php
+       $resultado = $_GET['exito'];
+       $paymentId = $_GET['paymentId'];
+       $id_pago = (int) $_GET['id_pago'];
+       $pagado = 1;
+
+       if ($resultado == 'true') {
+           echo '<div class="resultado correcto">';
+           echo 'El Pago se realizo correctamente<br>';
+           echo 'El ID es: '.$paymentId;
+           echo '</div>';
+
+           require 'includes/functions/db_conection.php';
+           $ps = $conn->prepare('UPDATE registro set pagado = ? where id_registro = ?');
+           $ps->bind_param('ii', $pagado, $id_pago);
+           $ps->execute();
+           $ps->close();
+           $conn->close();
+       } else {
+           echo '<div class="resultado error">';
+           echo 'El Pago no se realizo';
+           echo '</div>';
+       }
+    ?> 
     
 </section>
 
