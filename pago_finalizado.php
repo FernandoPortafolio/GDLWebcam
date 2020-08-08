4494a97a357856a7426ec96aaaa55d1cd2e4c67a
Fernando Acosta <Fernando Acosta>
2020-08-08 18:55:28 -0500

    manejo de errores en finalizar_pago.php

--- pago_finalizado.php
+++ pago_finalizado.php
@@ -14,36 +14,39 @@
        $id_pago = (int) $_GET['id_pago'];
        $pagado = 1;
 
-       //Estas variables las manda Paypal en automatico
-       $paymentId = $_GET['paymentId'];
-       $payerID = $_GET['PayerID'];
+       try {
+           //Estas variables las manda Paypal en automatico
+           $paymentId = $_GET['paymentId'];
+           $payerID = $_GET['PayerID'];
 
-       //Peticion a REST API de Paypal
-       $payment = Payment::get($paymentId, $apiContext);
-       $execution = new PaymentExecution();
-       $execution->setPayerId($payerID);
+           //Peticion a REST API de Paypal
+           $payment = Payment::get($paymentId, $apiContext);
+           $execution = new PaymentExecution();
+           $execution->setPayerId($payerID);
 
-       //Resultado tiene informacion de la transaccion
-       $result = $payment->execute($execution, $apiContext);
-       $respuesta = $result->transactions[0]->related_resources[0]->sale->state;
+           //Resultado tiene informacion de la transaccion
+           $result = $payment->execute($execution, $apiContext);
+           $respuesta = $result->transactions[0]->related_resources[0]->sale->state;
 
-       if ($respuesta == 'completed') {
-           echo '<div class="resultado correcto">';
-           echo 'El Pago se realizo correctamente<br>';
-           echo 'El ID es: '.$paymentId;
-           echo '</div>';
+           if ($respuesta == 'completed') {
+               echo '<div class="resultado correcto">';
+               echo 'El Pago se realizo correctamente<br>';
+               echo 'El ID es: '.$paymentId;
+               echo '</div>';
 
-           require 'includes/functions/db_conection.php';
-           $ps = $conn->prepare('UPDATE registro set pagado = ? where id_registro = ?');
-           $ps->bind_param('ii', $pagado, $id_pago);
-           $ps->execute();
-           $ps->close();
-           $conn->close();
-       } else {
+               require 'includes/functions/db_conection.php';
+               $ps = $conn->prepare('UPDATE registro set pagado = ? where id_registro = ?');
+               $ps->bind_param('ii', $pagado, $id_pago);
+               $ps->execute();
+               $ps->close();
+               $conn->close();
+           }
+       } catch (Throwable $th) {
            echo '<div class="resultado error">';
            echo 'El Pago no se realizo';
            echo '</div>';
        }
+
     ?> 
     
 </section>
