058eeaf8bbf0ad1ae2cd5cb234015ea1e2551dd4
Fernando Acosta <Fernando Acosta>
2020-08-08 22:31:54 -0500

    fix calendario footer h3

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,20 +1,20 @@
 <footer class="site-footer">
     <div class="contenedor contenido">
         <div class="footer-info">
-            <h3>Sobre <span>GdlWebcamp</span></h3>
+            <h3 class='footer-h3'>Sobre <span>GdlWebcamp</span></h3>
             <p>Proident ipsum incididunt fugiat cillum Lorem adipisicing mollit magna occaecat laboris
                 dolor ipsum. Duis ut ullamco magna cillum. Nulla laboris nostrud pariatur adipisicing proident
                 officia excepteur quis tempor dolore quis labore eu. Reprehenderit cillum duis duis aliquip irure
                 tempor tempor aliquip. </p>
         </div>
         <div class="ultimos-tweets">
-            <h3>Últimos <span>Tweets</span></h3>
+            <h3 class='footer-h3'>Últimos <span>Tweets</span></h3>
             <p>Duis consequat proident <span>#aute</span> aliqua amet cillum.</p>
             <p>Voluptate amet est <span>#ipsum</span> excepteur in consectetur velit irure in sit nulla.</p>
             <p>Excepteur ut dolore aliqua duis laborum aute <span>#exercitation</span> ea mollit.</p>
         </div>
         <div class="menu">
-            <h3>Redes <span>Sociales</span></h3>
+            <h3 class='footer-h3'>Redes <span>Sociales</span></h3>
             <nav class="redes-sociales">
                 <a href="#"><i class="fab fa-facebook-f"></i></a>
                 <a href="#"><i class="fab fa-twitter"></i></a>
